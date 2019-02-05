657bedb65ac6eed08f23e510ed8233d31a6d9745
Harianto van Insulinde <harianto.v.insulinde@valtech.nl>
2019-02-05 11:34:27 +0100

    axiosBluebird

---
+++ axiosBluebird.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+const Promise = require('./lib/Promise')
+const { stringify } = require('query-string')
+
+const axiosBluebird = {
+  Promise,
+  get: (url, params) => new Promise((fulfil, reject, onCancel) => { // eslint-disable-line
+    const cancelSource = axios.CancelToken.source()
+    const cancelToken = cancelSource.token
+
+    const config = {
+      params,
+      cancelToken,
+      paramsSerializer: stringify
+    }
+
+    axios
+      .get(url, config)
+      .then(fulfil)
+      .catch(reject)
+
+    onCancel(() => {
+      cancelSource.cancel()
+    })
+  }),
+  post: (url, params) => new Promise((fulfil, reject, onCancel) => { // eslint-disable-line
+    const cancelSource = axios.CancelToken.source()
+    const cancelToken = cancelSource.token
+
+    axios
+      .post(url, params, { cancelToken })
+      .then(fulfil)
+      .catch(reject)
+
+    onCancel(() => {
+      cancelSource.cancel()
+    })
+  })
+}
+
+module.exports = axiosBluebird
