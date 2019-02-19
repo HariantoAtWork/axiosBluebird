a79de5061c1c128cf1f3dc91903febb6a8616e20
Harianto van Insulinde <harianto.v.insulinde@valtech.nl>
2019-02-19 13:36:29 +0100

    Babel to JS: 1.1.0

--- index.js
+++ index.js
@@ -1,43 +1,68 @@
-'use strict';
+'use strict'
 
-var axios = require('axios');
-var Promise = require('./lib/Promise');
+var _axios = require('axios')
+
+var Promise = require('./lib/Promise')
 
 var _require = require('query-string'),
-    stringify = _require.stringify;
+  stringify = _require.stringify
 
+var methodsNoData = ['delete', 'get', 'head', 'options']
+var methodsWithData = ['post', 'put', 'patch']
 var axiosBluebird = {
   Promise: Promise,
-  get: function get(url, params) {
-    return new Promise(function (fulfil, reject, onCancel) { // eslint-disable-line
-      var cancelSource = axios.CancelToken.source();
-      var cancelToken = cancelSource.token;
+  axios: function axios(configRequest) {
+    return new Promise(function (fulfil, reject, onCancel) {
+      // eslint-disable-line
+      var cancelSource = _axios.CancelToken.source()
 
+      var cancelToken = configRequest.hasOwnProperty('cancelToken') ? configRequest.cancelToken : cancelSource.token
+      var requestConfig = { ...configRequest,
+        ...{
+          cancelToken: cancelToken
+        }
+      }
+      onCancel(function () {
+        cancelSource.cancel()
+      })
+      return _axios(requestConfig).then(fulfil).catch(reject)
+    })
+  }
+}
+methodsNoData.forEach(function (method) {
+  axiosBluebird[method] = function (url, params) {
+    return new Promise(function (fulfil, reject, onCancel) {
+      // eslint-disable-line
+      var cancelSource = _axios.CancelToken.source()
+
+      var cancelToken = cancelSource.token
       var config = {
         params: params,
         cancelToken: cancelToken,
         paramsSerializer: stringify
-      };
-
-      axios.get(url, config).then(fulfil).catch(reject);
-
+      }
       onCancel(function () {
-        cancelSource.cancel();
-      });
-    });
-  },
-  post: function post(url, params) {
-    return new Promise(function (fulfil, reject, onCancel) { // eslint-disable-line
-      var cancelSource = axios.CancelToken.source();
-      var cancelToken = cancelSource.token;
-
-      axios.post(url, params, { cancelToken: cancelToken }).then(fulfil).catch(reject);
-
-      onCancel(function () {
-        cancelSource.cancel();
-      });
-    });
+        cancelSource.cancel()
+      })
+      return _axios[method](url, config).then(fulfil).catch(reject)
+    })
   }
-};
+})
+methodsWithData.forEach(function (method) {
+  axiosBluebird[method] = function (url, params) {
+    return new Promise(function (fulfil, reject, onCancel) {
+      // eslint-disable-line
+      var cancelSource = _axios.CancelToken.source()
 
-module.exports = axiosBluebird;
+      var cancelToken = cancelSource.token
+      onCancel(function () {
+        cancelSource.cancel()
+      })
+      return _axios[method](url, params, {
+        cancelToken: cancelToken
+      }).then(fulfil).catch(reject)
+    })
+  }
+})
+
+module.exports = axiosBluebird
