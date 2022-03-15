a1696370109c72e4b886624969b6a4ed550852f8
Harianto van Insulinde <hariantoatwork@gmail.com>
2022-03-15 11:05:58 +0100

    NPM did not get Promise from Bluebird

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict";
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -22,14 +24,14 @@ var axiosBluebird = {
 
       var cancelToken = configRequest.hasOwnProperty('cancelToken') ? configRequest.cancelToken : cancelSource.token;
 
-      var requestConfig = _objectSpread({}, configRequest, {
+      var requestConfig = _objectSpread({}, configRequest, {}, {
         cancelToken: cancelToken
       });
 
       onCancel(function () {
         cancelSource.cancel();
       });
-      return _axios(requestConfig).then(fulfil).catch(reject);
+      return _axios(requestConfig).then(fulfil)["catch"](reject);
     });
   }
 };
@@ -48,7 +50,7 @@ methodsNoData.forEach(function (method) {
       onCancel(function () {
         cancelSource.cancel();
       });
-      return _axios[method](url, config).then(fulfil).catch(reject);
+      return _axios[method](url, config).then(fulfil)["catch"](reject);
     });
   };
 });
@@ -64,7 +66,7 @@ methodsWithData.forEach(function (method) {
       });
       return _axios[method](url, params, {
         cancelToken: cancelToken
-      }).then(fulfil).catch(reject);
+      }).then(fulfil)["catch"](reject);
     });
   };
 });
